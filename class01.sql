aae64492d856d0f2c567cbb0c0365846773c3fc0
Jeced <jecedlora@gmail.com>
2019-08-12 00:10:04 -0500

    Create class01.sql

---
+++ class01.sql
@@ -0,0 +1,13 @@
+use master
+create database class01
+
+use class01
+
+create table Empleado(
+rfc_empleado varchar(13)
+nombre_empleado varchar(30)
+direccion_empleado varchar(30)
+telefono_empleado varchar(13)
+puesto varchar(10)
+
+);
