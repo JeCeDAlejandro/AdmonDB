6421393e782f007f99c8959a2b18aceae92d98fa
Jeced <jecedlora@gmail.com>
2019-08-12 10:52:12 -0500

    Update and rename class01.sql to la_esperanza.sql

--- la_esperanza.sql
+++ la_esperanza.sql
@@ -4,11 +4,11 @@ create database la_esperanza
 use la_esperanza
 
 create table sucursal (
-  numero_sucursal       INT				NOT NULL,
+  numero_sucursal       INT			NOT NULL,
   nombre_sucursal       VARCHAR(20)		NOT NULL,
   direccion_sucursal    VARCHAR(20)		NOT NULL,
   telefono_sucursal     VARCHAR(20)		NOT NULL,
-  gerente				VARCHAR(20)		NOT NULL
+  gerente		VARCHAR(20)		NOT NULL
 );
 create table empleado (
   numero_empleado       VARCHAR(13)		NOT NULL,
@@ -16,49 +16,49 @@ create table empleado (
   direccion_empleado    VARCHAR(20)		NOT NULL,
   telefono_empleado     VARCHAR(20)		NOT NULL,
   puesto                VARCHAR(10)		NOT NULL,
-  numero_sucursal       INT				NOT NULL
+  numero_sucursal       INT			NOT NULL
 );
 create table producto (
-  id_producto           INT				NOT NULL,
+  id_producto           INT			NOT NULL,
   descripcion           VARCHAR(50)		NOT NULL,
-  existencia            INT				NOT NULL,
+  existencia            INT			NOT NULL,
   precio_producto       FLOAT			NOT NULL,
   tamaño                VARCHAR(10)		NOT NULL,
   precio_compra         FLOAT			NOT NULL,
   precio_venta          FLOAT			NOT NULL
 );
 create table proveedor (
-  id_proveedor			VARCHAR(13)		NOT NULL,
+  id_proveedor		VARCHAR(13)		NOT NULL,
   nombre_proveedor      VARCHAR(30)		NOT NULL,
   direccion_proveedor   VARCHAR(20)		NOT NULL,
   telefono_proveedor    VARCHAR(20)		NOT NULL,
   correo                VARCHAR(30)		NOT NULL
 );
 create table venta (
-  numero_venta          INT				NOT NULL,
+  numero_venta          INT			NOT NULL,
   fecha_venta           DATE			NOT NULL,
   hora_venta            TIME			NOT NULL,
   forma_pago            VARCHAR(10)		NOT NULL,
   numero_empleado       VARCHAR(13)		NOT NULL
 );
 create table compra (
-  numero_compra         INT				NOT NULL,
+  numero_compra         INT			NOT NULL,
   fecha_compra          DATE			NOT NULL,
   hora_compra           TIME			NOT NULL,
   forma_pago            VARCHAR(10)		NOT NULL,
   numero_empleado       VARCHAR(13)		NOT NULL,
-  id_proveedor			VARCHAR(13)		NOT NULL
+  id_proveedor		VARCHAR(13)		NOT NULL
 );
 
 create table producto_venta (
-  numero_venta          INT				NOT NULL,
-  id_producto           INT				NOT NULL,
-  cantidad_venta        INT				NOT NULL
+  numero_venta          INT			NOT NULL,
+  id_producto           INT			NOT NULL,
+  cantidad_venta        INT			NOT NULL
 );
 create table producto_compra (
-  numero_compra         INT				NOT NULL,
-  id_producto           INT				NOT NULL,
-  cantidad_compra       INT				NOT NULL
+  numero_compra         INT			NOT NULL,
+  id_producto           INT			NOT NULL,
+  cantidad_compra       INT			NOT NULL
 );
 
 /*
