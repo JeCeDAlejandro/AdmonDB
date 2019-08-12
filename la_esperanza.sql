ce058e6c0b7da2e221c951cef1720890b98cc2f9
Jeced <jecedlora@gmail.com>
2019-08-12 11:17:09 -0500

    Update la_esperanza.sql

--- la_esperanza.sql
+++ la_esperanza.sql
@@ -1,14 +1,16 @@
 use master
 create database la_esperanza
+drop database la_esperanza
 
 use la_esperanza
+go
 
 create table sucursal (
-  numero_sucursal       INT			NOT NULL,
+  numero_sucursal       INT				NOT NULL,
   nombre_sucursal       VARCHAR(20)		NOT NULL,
   direccion_sucursal    VARCHAR(20)		NOT NULL,
   telefono_sucursal     VARCHAR(20)		NOT NULL,
-  gerente		VARCHAR(20)		NOT NULL
+  gerente				VARCHAR(20)		NOT NULL
 );
 create table empleado (
   numero_empleado       VARCHAR(13)		NOT NULL,
@@ -16,49 +18,49 @@ create table empleado (
   direccion_empleado    VARCHAR(20)		NOT NULL,
   telefono_empleado     VARCHAR(20)		NOT NULL,
   puesto                VARCHAR(10)		NOT NULL,
-  numero_sucursal       INT			NOT NULL
+  numero_sucursal       INT				NOT NULL
 );
 create table producto (
-  id_producto           INT			NOT NULL,
+  id_producto           INT				NOT NULL,
   descripcion           VARCHAR(50)		NOT NULL,
-  existencia            INT			NOT NULL,
+  existencia            INT				NOT NULL,
   precio_producto       FLOAT			NOT NULL,
   tamaño                VARCHAR(10)		NOT NULL,
   precio_compra         FLOAT			NOT NULL,
   precio_venta          FLOAT			NOT NULL
 );
 create table proveedor (
-  id_proveedor		VARCHAR(13)		NOT NULL,
+  id_proveedor			VARCHAR(13)		NOT NULL,
   nombre_proveedor      VARCHAR(30)		NOT NULL,
   direccion_proveedor   VARCHAR(20)		NOT NULL,
   telefono_proveedor    VARCHAR(20)		NOT NULL,
   correo                VARCHAR(30)		NOT NULL
 );
 create table venta (
-  numero_venta          INT			NOT NULL,
+  numero_venta          INT				NOT NULL,
   fecha_venta           DATE			NOT NULL,
   hora_venta            TIME			NOT NULL,
   forma_pago            VARCHAR(10)		NOT NULL,
   numero_empleado       VARCHAR(13)		NOT NULL
 );
 create table compra (
-  numero_compra         INT			NOT NULL,
+  numero_compra         INT				NOT NULL,
   fecha_compra          DATE			NOT NULL,
   hora_compra           TIME			NOT NULL,
   forma_pago            VARCHAR(10)		NOT NULL,
   numero_empleado       VARCHAR(13)		NOT NULL,
-  id_proveedor		VARCHAR(13)		NOT NULL
+  id_proveedor			VARCHAR(13)		NOT NULL
 );
 
 create table producto_venta (
-  numero_venta          INT			NOT NULL,
-  id_producto           INT			NOT NULL,
-  cantidad_venta        INT			NOT NULL
+  numero_venta          INT				NOT NULL,
+  id_producto           INT				NOT NULL,
+  cantidad_venta        INT				NOT NULL
 );
 create table producto_compra (
-  numero_compra         INT			NOT NULL,
-  id_producto           INT			NOT NULL,
-  cantidad_compra       INT			NOT NULL
+  numero_compra         INT				NOT NULL,
+  id_producto           INT				NOT NULL,
+  cantidad_compra       INT				NOT NULL
 );
 
 /*
@@ -80,7 +82,7 @@ ALTER TABLE compra
 	add primary key(numero_compra)
 ALTER TABLE producto_venta
 	add primary key(numero_venta, id_producto)
-ALTER TABLE producto_venta
+ALTER TABLE producto_compra
 	add primary key(numero_compra, id_producto)
 
 /*
@@ -115,9 +117,19 @@ ALTER TABLE producto_venta
 	on update cascade
 ALTER TABLE producto_compra
 	add foreign key (numero_compra)
-	references venta (numero_compra)
+	references compra (numero_compra)
 	on update cascade
 ALTER TABLE producto_compra
 	add foreign key (id_producto)
 	references producto (id_producto)
 	on update cascade
+
+
+insert into sucursal
+	(numero_sucursal, nombre_sucursal, direccion_sucursal, telefono_sucursal, gerente)
+VALUES
+	('0', '', 'Avenida Siempreviva 742', '', ''),
+	('1', '', 'Whitehaven Mansions Apt 56B', '', ''),
+	('2', '', 'Calle Ignacio Rayon No.949', '', ''),
+	('3', '', 'Avenida Independencia No. 670', '', ''),
+	('4', '', 'Calle 21 De Marzo No. 17', '', '')
